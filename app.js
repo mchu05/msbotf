ec125fb2415d2fb57b00d8ba919b1860619c2ee4
Chu, Monica <monica.chu@accenture.com>
2017-06-08 14:30:14 -0400

    added search-demo

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.MY_APP_ID,
-    appPassword: process.env.MY_APP_PASSWORD
-};
+// This startup file is meant to be used only when deploying any of the samples to an Azure Website
 
-// Create bot
-var connector = new builder.ChatConnector(botConnectorOptions);
-var bot = new builder.UniversalBot(connector);
-
-bot.dialog('/', function (session) {
-    
-    //respond with user's message
-    //this will send you said+what ever user says.
-    session.send("You said " + session.message.text);
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-
-// Handle Bot Framework messages
-/*here we are giving path as "/api/messages" because during the process of regi9stering bot we have given end point URL as "azure qwebapp url/api/messages" if you want to give some other url give the same url whatever you give in the endpoint excluding azure webapp url */
-server.post('/api/messages', connector.listen());
-
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-        'directory': '.',
-        'default': 'index.html'
-}));
-
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
+var botName = process.env.BOT || 'RealEstateBot';
+var botModule = './' + botName + '/app'; 
+require(botModule);
