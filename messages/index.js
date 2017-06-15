e32e3ab1175045a30ae59bffa4fae6ed6dd2baa2
Chu, Monica <monica.chu@accenture.com>
2017-06-15 15:33:00 -0400

    added files into the main index.js

--- messages/index.js
+++ messages/index.js
@@ -2,6 +2,7 @@
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
+var request = require('request');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -12,8 +13,33 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
+var searchName = process.env.AZURE_SEARCH_NAME ? process.env.AZURE_SEARCH_NAME : "ethics";
+var indexName = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_NAME : "ethicsindex";
+var searchKey = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_KEY : "1B00A9C8F4FE8B7970E4249C12E141E4";
+
+var  queryString = 'https://' + searchName + '.search.windows.net/indexes/' + indexName + '/docs?api-key=' + searchKey + '&api-version=2015-02-28&';
+
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
+
+
+var searchQueryStringBuilder = function (query) {
+    return queryString + query;
+}
+
+var performSearchQuery = function (queryString, callback) {
+    console.log(queryString);
+    request(queryString, function (error, response, body) {
+        if (!error && response && response.statusCode == 200) {
+            var result = JSON.parse(body);
+            callback(null, result);
+        } else {
+            callback(error, null);
+        }
+    })
+}
+
+
 
 bot.dialog('/', [
     function (session) {
@@ -47,71 +73,54 @@ bot.dialog('/promptButtons', [
 
 bot.dialog('/search', [
         function (session) {
+            //Prompt for string input
             builder.Prompts.text(session, "Type in something you are searching for");
         },
         function (session, results) {
             //Sets name equal to resulting input
             var name = results.response;
 
-            // var queryString = searchQueryStringBuilder('search= ' + name);
-            // performSearchQuery(queryString, function (err, result) {
-            //     if (err) {
-            //         console.log("Error when searching: " + err);
-            //     } else if (result && result['value'] && result['value'][0]) {
-            //         //If we have results send them to the showResults dialog (acts like a decoupled view)
-            //         session.replaceDialog('/showResults', { result });
-            //     } else {
-            //         session.endDialog("Nothing by \'" + name + "\' found");
-            //     }
-            // })
-
-            session.endDialog("Nothing by \'" + name + "\' found");
+            var queryString = searchQueryStringBuilder('search= ' + name);
+            performSearchQuery(queryString, function (err, result) {
+                if (err) {
+                    console.log("Error when searching: " + err);
+                } else if (result && result['value'] && result['value'][0]) {
+                    //If we have results send them to the showResults dialog (acts like a decoupled view)
+                    session.replaceDialog('/showResults', { result });
+                } else {
+                    session.endDialog("Nothing by \'" + name + "\' found");
+                }
+            })
         }
 ]);
 
 bot.dialog('/explore', [
         function (session) {
             builder.Prompts.text(session, "Type in category");
-            // //Syntax for faceting results by 'Era'
-            // var queryString = searchQueryStringBuilder('facet=Era');
-            // performSearchQuery(queryString, function (err, result) {
-            //     if (err) {
-            //         console.log("Error when faceting by era:" + err);
-            //     } else if (result && result['@search.facets'] && result['@search.facets'].Era) {
-            //         eras = result['@search.facets'].Era;
-            //         var eraNames = [];
-            //         //Pushes the name of each era into an array
-            //         eras.forEach(function (era, i) {
-            //             eraNames.push(era['value'] + " (" + era.count + ")");
-            //         })    
-            //         //Prompts the user to select the era he/she is interested in
-            //         builder.Prompts.choice(session, "Which era of music are you interested in?", eraNames);
-            //     } else {
-            //         session.endDialog("I couldn't find any genres to show you");
-            //     }
-            // })
+            //seach from tags
         },
         function (session, results) {
+            //implementation
             session.endDialog("I couldn't find that category ");
-
-            // //Chooses just the era name - parsing out the count
-            // var era = results.response.entity.split(' ')[0];;
-
-            // //Syntax for filtering results by 'era'. Note the $ in front of filter (OData syntax)
-            // var queryString = searchQueryStringBuilder('$filter=Era eq ' + '\'' + era + '\'');
-
-            // performSearchQuery(queryString, function (err, result) {
-            //     if (err) {
-            //         console.log("Error when filtering by genre: " + err);
-            //     } else if (result && result['value'] && result['value'][0]) {
-            //         //If we have results send them to the showResults dialog (acts like a decoupled view)
-            //         session.replaceDialog('/showResults', { result });
-            //     } else {
-            //         session.endDialog("I couldn't find any musicians in that era :0");
-            //     }
-            // })
         }
 ]);
+
+bot.dialog('/showResults', [
+        function (session, args) {
+            var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel);
+                args.result['value'].forEach(function (item, i) {
+                    msg.addAttachment(
+                        new builder.HeroCard(session)
+                            .title(item.title)
+                            .subtitle("Search Score: " + item['@search.score'])
+                            .text(item.description)
+                            .images([builder.CardImage.create(session, item.imageUrl)])
+                    );
+                })
+                session.endDialog(msg);
+        }
+]);
+
 
 if (useEmulator) {
     var restify = require('restify');
