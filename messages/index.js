5c44507065d2c0f8337d240f26eb9ce2567e00ef
Chu, Monica <monica.chu@accenture.com>
2017-06-15 14:40:44 -0400

    pla

--- messages/index.js
+++ messages/index.js
@@ -3,10 +3,6 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 
-require('./dialogs/results.js')(); 
-require('./dialogs/musicianExplorer.js')();
-require('./dialogs/musicianSearch.js')();
-
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -49,30 +45,73 @@ bot.dialog('/promptButtons', [
     }
 ]);
 
-// bot.dialog('/search', [
-//         function (session) {
-//             //Prompt for string input
-//             builder.Prompts.text(session, "Type in something you are searching for");
-//         },
-//         function (session, results) {
-//             //Sets name equal to resulting input
-//             var name = results.response;
+bot.dialog('/search', [
+        function (session) {
+            builder.Prompts.text(session, "Type in something you are searching for");
+        },
+        function (session, results) {
+            //Sets name equal to resulting input
+            var name = results.response;
 
-//             var queryString = searchQueryStringBuilder('search= ' + name);
-//             performSearchQuery(queryString, function (err, result) {
-//                 if (err) {
-//                     console.log("Error when searching: " + err);
-//                 } else if (result && result['value'] && result['value'][0]) {
-//                     //If we have results send them to the showResults dialog (acts like a decoupled view)
-//                     session.replaceDialog('/showResults', { result });
-//                 } else {
-//                     session.endDialog("Nothing by \'" + name + "\' found");
-//                 }
-//             })
-//         }
-// ]);
+            // var queryString = searchQueryStringBuilder('search= ' + name);
+            // performSearchQuery(queryString, function (err, result) {
+            //     if (err) {
+            //         console.log("Error when searching: " + err);
+            //     } else if (result && result['value'] && result['value'][0]) {
+            //         //If we have results send them to the showResults dialog (acts like a decoupled view)
+            //         session.replaceDialog('/showResults', { result });
+            //     } else {
+            //         session.endDialog("Nothing by \'" + name + "\' found");
+            //     }
+            // })
 
+            session.endDialog("Nothing by \'" + name + "\' found");
+        }
+]);
 
+bot.dialog('/explore', [
+        function (session) {
+            builder.Prompts.text(session, "Type in category");
+            // //Syntax for faceting results by 'Era'
+            // var queryString = searchQueryStringBuilder('facet=Era');
+            // performSearchQuery(queryString, function (err, result) {
+            //     if (err) {
+            //         console.log("Error when faceting by era:" + err);
+            //     } else if (result && result['@search.facets'] && result['@search.facets'].Era) {
+            //         eras = result['@search.facets'].Era;
+            //         var eraNames = [];
+            //         //Pushes the name of each era into an array
+            //         eras.forEach(function (era, i) {
+            //             eraNames.push(era['value'] + " (" + era.count + ")");
+            //         })    
+            //         //Prompts the user to select the era he/she is interested in
+            //         builder.Prompts.choice(session, "Which era of music are you interested in?", eraNames);
+            //     } else {
+            //         session.endDialog("I couldn't find any genres to show you");
+            //     }
+            // })
+        },
+        function (session, results) {
+            session.endDialog("I couldn't find that category ");
+
+            // //Chooses just the era name - parsing out the count
+            // var era = results.response.entity.split(' ')[0];;
+
+            // //Syntax for filtering results by 'era'. Note the $ in front of filter (OData syntax)
+            // var queryString = searchQueryStringBuilder('$filter=Era eq ' + '\'' + era + '\'');
+
+            // performSearchQuery(queryString, function (err, result) {
+            //     if (err) {
+            //         console.log("Error when filtering by genre: " + err);
+            //     } else if (result && result['value'] && result['value'][0]) {
+            //         //If we have results send them to the showResults dialog (acts like a decoupled view)
+            //         session.replaceDialog('/showResults', { result });
+            //     } else {
+            //         session.endDialog("I couldn't find any musicians in that era :0");
+            //     }
+            // })
+        }
+]);
 
 if (useEmulator) {
     var restify = require('restify');
